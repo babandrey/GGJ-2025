bb7a196d500b1ead4419cf7e42285aa5823c412d
fkeyz <fkeyzuwu@gmail.com>
2025-01-24 01:52:30 +0200

    added slimelett stop at anchor point

--- Assets/Slimelett.cs
+++ Assets/Slimelett.cs
@@ -7,9 +7,20 @@ public class Slimelett : MonoBehaviour
     [SerializeField] private new Rigidbody2D rigidbody;
     [SerializeField] private float speed = 150.0f;
     public Vector3 walkDirection = Vector3.zero;
+    private bool isMoving = true;
     void FixedUpdate()
     {
+        if (!isMoving) return;
+
         Vector3 velocity = walkDirection * Time.deltaTime * speed;
         rigidbody.velocity = new Vector3(velocity.x, rigidbody.velocity.y, velocity.z);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("AnchorPoint"))
+        {
+            isMoving = false;
+        }
     }
 }
