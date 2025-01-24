99ee3f68d6c83ba8524afe1becff00fba0a3d774
fkeyz <fkeyzuwu@gmail.com>
2025-01-24 11:49:33 +0200

    added merging

--- Assets/Scripts/Slimelett.cs
+++ Assets/Scripts/Slimelett.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class Slimelett : MonoBehaviour
 {
     [SerializeField] private new Rigidbody2D rigidbody;
+    [SerializeField] private CircleCollider2D circleCollider;
     [SerializeField] private float speed = 150.0f;
-    public Vector3 walkDirection = Vector3.zero;
+    [HideInInspector] public Vector3 walkDirection = Vector3.zero;
     private bool isMoving = true;
+    private bool isMerging = false;
+    [HideInInspector] public float lifeTime = 0.0f;
     void FixedUpdate()
     {
+        lifeTime += Time.fixedDeltaTime;
+
         if (!isMoving) return;
 
         Vector3 velocity = walkDirection * Time.deltaTime * speed;
@@ -22,5 +27,22 @@ public class Slimelett : MonoBehaviour
         {
             isMoving = false;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("hewo");
+        // moving slimelett should be the one to merge into stationary slimelett
+        if (collision.gameObject.TryGetComponent(out Slimelett slimelett) && this.lifeTime < slimelett.lifeTime)
+        {
+            MergeToSlimelett(slimelett);
+        }
+    }
+
+    void MergeToSlimelett(Slimelett other)
+    {
+        this.rigidbody.simulated = false;
+        this.transform.LeanMove(other.transform.position, 0.25f).setEaseInCubic().setDestroyOnComplete(true);
+        other.transform.LeanScale(other.transform.localScale + this.transform.localScale, 0.25f).setEaseInCubic();
     }
 }
